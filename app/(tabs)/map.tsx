23c75858bf8f486b609f424eefe8c9dbb32a7fcc
Gayathra Dissanayake <106143446+ItsAeox@users.noreply.github.com>
2024-12-01 02:50:35 +0530

    disaster map working

--- app/(tabs)/map.tsx
+++ app/(tabs)/map.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -9,14 +9,38 @@ import {
   Alert,
 } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Polygon } from 'react-native-maps';
+import axios from 'axios';
+import icons from '@/constants/Icons';
 import { useRouter } from 'expo-router';
 import { Double } from 'react-native/Libraries/Types/CodegenTypes';
-import axios from 'axios';
+
+// Define TypeScript types
+interface Disaster {
+  latitude: number;
+  longitude: number;
+  radius: number;
+  type: string;
+}
+
+interface Hospital {
+  latitude: number;
+  longitude: number;
+  name: string;
+}
+
+interface Shelter {
+  latitude: number;
+  longitude: number;
+  name: string;
+}
 
 const MapPage: React.FC = () => {
-  const router = useRouter();
+    const router = useRouter();
   const [searchText, setSearchText] = useState('');
+  const [disasters, setDisasters] = useState<Disaster[]>([]);
+  const [hospitals, setHospitals] = useState<Hospital[]>([]);
+  const [shelters, setShelters] = useState<Shelter[]>([]);
   const [selectedDisaster, setSelectedDisaster] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
   const [locationPickerVisible, setLocationPickerVisible] = useState(false);
@@ -85,6 +109,74 @@ const MapPage: React.FC = () => {
     }
   };
 
+
+  useEffect(() => {
+    const fetchMapData = async () => {
+      try {
+        const response = await axios.get('http://192.168.1.14:8080/api/users/map');
+        if (response.status === 200) {
+          const { disasters, hospitals, shelters } = response.data;
+          setDisasters(disasters || []);
+          setHospitals(hospitals || []);
+          setShelters(shelters || []);
+        } else {
+          Alert.alert('Error', 'Unable to fetch map data.');
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          console.error(error.message);
+        } else {
+          console.error('Unknown error:', error);
+        }
+      }
+    };
+
+    fetchMapData();
+  }, []);
+
+  const generateCirclePoints = (latitude: number, longitude: number, radius: number, points = 36) => {
+    const earthRadius = 6371000; // Earth radius in meters
+    const circlePoints = [];
+    const lat = (latitude * Math.PI) / 180;
+    const lng = (longitude * Math.PI) / 180;
+  
+    for (let i = 0; i <= points; i++) {
+      const angle = (i * 360) / points;
+      const angleRad = (angle * Math.PI) / 180;
+  
+      const latOffset = Math.asin(
+        Math.sin(lat) * Math.cos(radius / earthRadius) +
+        Math.cos(lat) * Math.sin(radius / earthRadius) * Math.cos(angleRad)
+      );
+      const lngOffset =
+        lng +
+        Math.atan2(
+          Math.sin(angleRad) * Math.sin(radius / earthRadius) * Math.cos(lat),
+          Math.cos(radius / earthRadius) - Math.sin(lat) * Math.sin(latOffset)
+        );
+  
+      circlePoints.push({
+        latitude: (latOffset * 180) / Math.PI,
+        longitude: (lngOffset * 180) / Math.PI,
+      });
+    }
+    return circlePoints;
+  };
+  
+
+  const getDisasterColors = (type: string) => {
+    switch (type) {
+      case 'Flood':
+        return { fillColor: 'rgba(0, 0, 255, 0.2)', strokeColor: 'rgba(0, 0, 255, 0.8)' };
+      case 'hurricane':
+        return { fillColor: 'rgba(255, 0, 0, 0.2)', strokeColor: 'rgba(255, 0, 0, 0.8)' };
+      case 'landslide':
+        return { fillColor: 'rgba(0, 255, 0, 0.2)', strokeColor: 'rgba(0, 255, 0, 0.8)' };
+      default:
+        return { fillColor: 'rgba(0, 0, 0, 0.2)', strokeColor: 'rgba(0, 0, 0, 0.8)' };
+    }
+  };
+
   return (
     <View style={styles.container}>
       <MapView
@@ -98,6 +190,42 @@ const MapPage: React.FC = () => {
         onPress={locationPickerVisible ? handleMapPress : undefined}
       >
         {selectedLocation && <Marker coordinate={selectedLocation} />}
+
+        {disasters.map((disaster, index) => {
+          const { latitude, longitude, radius, type } = disaster;
+          const { fillColor, strokeColor } = getDisasterColors(type);
+          const circlePoints = generateCirclePoints(latitude, longitude, radius);
+          return (
+            <Polygon
+              key={index}
+              coordinates={circlePoints}
+              fillColor={fillColor}
+              strokeColor={strokeColor}
+              strokeWidth={2}
+            />
+          );
+        })}
+
+        {hospitals.map((hospital, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: hospital.latitude, longitude: hospital.longitude }}
+            title={hospital.name}
+            description="Hospital"
+            icon={icons.Hospital}
+          />
+        ))}
+
+        {shelters.map((shelter, index) => (
+          <Marker
+            key={index}
+            coordinate={{ latitude: shelter.latitude, longitude: shelter.longitude }}
+            title={shelter.name}
+            description="Shelter"
+            pinColor="orange"
+            icon={icons.Shelter}
+          />
+        ))}
       </MapView>
 
       <View style={styles.searchContainer}>
@@ -105,13 +233,12 @@ const MapPage: React.FC = () => {
           style={styles.searchBar}
           placeholder="Search locations..."
           value={searchText}
-          onChangeText={handleSearchChange}
+          onChangeText={setSearchText}
         />
-        <TouchableOpacity style={styles.searchButton} onPress={performSearch}>
+        <TouchableOpacity style={styles.searchButton} onPress={() => Alert.alert('Search', searchText)}>
           <FontAwesome5 name="search" size={18} color="#007B70" />
         </TouchableOpacity>
       </View>
-
       {/* Add Disaster Button (hidden when in modal or location picker) */}
       {!modalVisible && !locationPickerVisible && (
         <TouchableOpacity
@@ -204,158 +331,157 @@ const MapPage: React.FC = () => {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
-  searchContainer: {
-    position: 'absolute',
-    top: 50,
-    left: 10,
-    right: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    zIndex: 2,
-  },
-  searchBar: {
-    flex: 1,
-    backgroundColor: '#fff',
-    borderColor: '#007B70',
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-    paddingRight: 40,
-  },
-  searchButton: {
-    position: 'absolute',
-    right: 20,
-    top: '50%',
-    transform: [{ translateY: -9 }],
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  disasterButton: {
-    position: 'absolute',
-    bottom: 60,
-    right: 20,
-    backgroundColor: '#007B70',
-    borderRadius: 5,
-    paddingVertical: 10,
-    paddingHorizontal: 15,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  disasterButtonText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  },
-  modalContent: {
-    width: '80%',
-    backgroundColor: '#fff',
-    borderRadius: 10,
-    padding: 20,
-    alignItems: 'center',
-  },
-  modalTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  disasterOption: {
-    width: '100%',
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#007B70',
-    borderRadius: 5,
-    marginBottom: 10,
-    alignItems: 'center',
-  },
-  selectedDisasterOption: {
-    backgroundColor: '#007B70',
-  },
-  disasterText: {
-    color: '#007B70',
-    fontSize: 16,
-  },
-  selectedDisasterText: {
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-  modalActions: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-    marginTop: 20,
-  },
-  cancelButton: {
-    flex: 1,
-    marginRight: 5,
-    backgroundColor: '#ccc',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-  cancelButtonText: {
-    color: '#000',
-    fontSize: 16,
-  },
-  nextButton: {
-    flex: 1,
-    marginLeft: 5,
-    backgroundColor: '#007B70',
-    padding: 10,
-    borderRadius: 5,
-    alignItems: 'center',
-  },
-  nextButtonText: {
-    color: '#fff',
-    fontSize: 16,
-  },
-  locationPicker: {
-    position: 'absolute',
-    bottom: 100,
-    left: 10,
-    right: 10,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    padding: 15,
-    alignItems: 'center',
-  },
-  pickerText: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  locationText: {
-    fontSize: 14,
-    marginBottom: 10,
-  },
-  pickerActions: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    width: '100%',
-  },
-  submitButton: {
-    flex: 1,
-    backgroundColor: '#007B70',
-    borderRadius: 5,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    marginLeft: 5,
-  },
-  submitButtonText: {
-    color: '#fff',
-    fontSize: 16,
-    textAlign: 'center',
-  },
-});
-
+    container: {
+      flex: 1,
+    },
+    map: {
+      width: '100%',
+      height: '100%',
+    },
+    searchContainer: {
+      position: 'absolute',
+      top: 50,
+      left: 10,
+      right: 10,
+      flexDirection: 'row',
+      alignItems: 'center',
+      zIndex: 2,
+    },
+    searchBar: {
+      flex: 1,
+      backgroundColor: '#fff',
+      borderColor: '#007B70',
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+      paddingRight: 40,
+    },
+    searchButton: {
+      position: 'absolute',
+      right: 20,
+      top: '50%',
+      transform: [{ translateY: -9 }],
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    disasterButton: {
+      position: 'absolute',
+      bottom: 60,
+      right: 20,
+      backgroundColor: '#007B70',
+      borderRadius: 5,
+      paddingVertical: 10,
+      paddingHorizontal: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    disasterButtonText: {
+      color: '#fff',
+      fontSize: 16,
+    },
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    },
+    modalContent: {
+      width: '80%',
+      backgroundColor: '#fff',
+      borderRadius: 10,
+      padding: 20,
+      alignItems: 'center',
+    },
+    modalTitle: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    disasterOption: {
+      width: '100%',
+      padding: 10,
+      borderWidth: 1,
+      borderColor: '#007B70',
+      borderRadius: 5,
+      marginBottom: 10,
+      alignItems: 'center',
+    },
+    selectedDisasterOption: {
+      backgroundColor: '#007B70',
+    },
+    disasterText: {
+      color: '#007B70',
+      fontSize: 16,
+    },
+    selectedDisasterText: {
+      color: '#fff',
+      fontWeight: 'bold',
+    },
+    modalActions: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '100%',
+      marginTop: 20,
+    },
+    cancelButton: {
+      flex: 1,
+      marginRight: 5,
+      backgroundColor: '#ccc',
+      padding: 10,
+      borderRadius: 5,
+      alignItems: 'center',
+    },
+    cancelButtonText: {
+      color: '#000',
+      fontSize: 16,
+    },
+    nextButton: {
+      flex: 1,
+      marginLeft: 5,
+      backgroundColor: '#007B70',
+      padding: 10,
+      borderRadius: 5,
+      alignItems: 'center',
+    },
+    nextButtonText: {
+      color: '#fff',
+      fontSize: 16,
+    },
+    locationPicker: {
+      position: 'absolute',
+      bottom: 100,
+      left: 10,
+      right: 10,
+      backgroundColor: '#fff',
+      borderRadius: 5,
+      padding: 15,
+      alignItems: 'center',
+    },
+    pickerText: {
+      fontSize: 16,
+      marginBottom: 10,
+    },
+    locationText: {
+      fontSize: 14,
+      marginBottom: 10,
+    },
+    pickerActions: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '100%',
+    },
+    submitButton: {
+      flex: 1,
+      backgroundColor: '#007B70',
+      borderRadius: 5,
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      marginLeft: 5,
+    },
+    submitButtonText: {
+      color: '#fff',
+      fontSize: 16,
+      textAlign: 'center',
+    },
+  });
 export default MapPage;
--- constants/Icons.ts
+++ constants/Icons.ts
@@ -4,4 +4,6 @@ export default {
   ReceiveCash: require('@/assets/images/icons/Receive Cash.png'),
   ReceiveDollar: require('@/assets/images/icons/Receive Dollar.png'),
   Victim: require('@/assets/images/icons/Victim.png'),
+  Hospital: require('@/assets/images/icons/hospital.png'),
+  Shelter: require('@/assets/images/icons/shelter.png'),
 };
